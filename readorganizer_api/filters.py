8dcfe24659b67bb145b0850d6a11eb30e1cb5e86
Mirek Długosz <mirek@mirekdlugosz.com>
2021-07-11 12:07:26 +0200

    Filter entries by channel id

--- readorganizer_api/filters.py
+++ readorganizer_api/filters.py
@@ -3,6 +3,10 @@ from django_filters import rest_framework as drf_filters
 from readorganizer_api import models
 
 
+class NumberInFilter(drf_filters.BaseInFilter, drf_filters.NumberFilter):
+    pass
+
+
 class EntryFilter(drf_filters.FilterSet):
     published_time = drf_filters.IsoDateTimeFilter(
         field_name="published_time", lookup_expr="exact"
@@ -19,6 +23,8 @@ class EntryFilter(drf_filters.FilterSet):
     published_time__gte = drf_filters.IsoDateTimeFilter(
         field_name="published_time", lookup_expr="gte"
     )
+    channel = drf_filters.NumberFilter(field_name="channel_id", lookup_expr="exact")
+    channel__in = NumberInFilter(field_name="channel_id", lookup_expr="in")
 
     class Meta:
         model = models.Entry
