18821c40c7d5a9e74bf26cbc33d6100f9d7e4478
Mirek Długosz <mirek@mirekdlugosz.com>
2022-02-19 14:06:49 +0100

    Dockerfile improvements

--- containers/entrypoint.prod.sh
+++ containers/entrypoint.prod.sh
@@ -4,23 +4,37 @@
 set -euo pipefail
 trap 's=$?; echo >&2 "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
-if [ -z "${KUSTOSZ_DONT_WAIT_FOR_POSTGRES:+x}" ]; then
-    echo "Waiting for postgres..."
+# ensure we have db connection
+DJANGODB=$(mktemp --suffix=.json)
+dynaconf list -k DATABASES__default --output-flat -o "$DJANGODB" > /dev/null
 
-    # FIXME: czy mogę mieć te same zmienne co w dynaconf?
-    while ! nc -z $SQL_HOST $SQL_PORT; do
+if ! jq '.DATABASES__default.ENGINE' "$DJANGODB" |grep -q sqlite3 ; then
+    DB_HOST="$(jq '.DATABASES__default.HOST' "$DJANGODB")"
+    DB_PORT="$(jq '.DATABASES__default.PORT' "$DJANGODB")"
+    echo "Waiting for db"
+    while ! nc -z "$DB_HOST" "$DB_PORT"; do
         sleep 0.1
     done
-
-    echo "PostgreSQL started"
+    echo "db started"
+    unset DB_HOST
+    unset DB_PORT
 fi
+rm "$DJANGODB"
 
+# run migrate by default
 if [ -z "${KUSTOSZ_SKIP_MIGRATE:+x}" ]; then
     kustosz-manager migrate --noinput
 fi
 
+# run collectstatic by default
 if [ -z "${KUSTOSZ_SKIP_COLLECTSTATIC:+x}" ]; then
     kustosz-manager collectstatic
+fi
+
+# run celery and redis by default
+if [ -z "${KUSTOSZ_ORCHESTRATED:+x}" ]; then
+    supervisord -c $HOME/supervisor/supervisord.conf
+    supervisorctl -c $HOME/supervisor/supervisord.conf start redis-server
 fi
 
 exec "$@"
