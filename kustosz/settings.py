70bf4ac7ec29ddb1bf287dc14f64ca7e89dbb40d
Mirek Długosz <mirek@mirekdlugosz.com>
2022-03-07 23:20:24 +0100

    Read settings from ~/.config/kustosz/settings.yaml

--- kustosz/settings.py
+++ kustosz/settings.py
@@ -3,11 +3,19 @@ Django settings are managed by dynaconf - see settings.yaml in parent directory
 """
 from pathlib import Path
 
+_CONFIG_DIR = Path.home() / ".config/kustosz/"
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 import dynaconf  # noqa
 
 settings = dynaconf.DjangoDynaconf(
-    __name__, settings_files=[BASE_DIR / "settings.yaml", BASE_DIR / ".secrets.yaml"]
+    __name__,
+    settings_files=[
+        BASE_DIR / "settings.yaml",
+        BASE_DIR / ".secrets.yaml",
+        _CONFIG_DIR / "settings.yaml",
+        _CONFIG_DIR / ".secrets.yaml",
+    ],
 )  # noqa
