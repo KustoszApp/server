187bcba4a71de90d6348907fe9f752ac034643fe
Mirek Długosz <mirek@mirekdlugosz.com>
2022-03-20 14:16:24 +0100

    Use KUSTOSZ_BASE_DIR as root for everything runtime-related

--- kustosz/settings.py
+++ kustosz/settings.py
@@ -1,21 +1,24 @@
 """
 Django settings are managed by dynaconf - see settings.yaml in parent directory
 """
+import os
 from pathlib import Path
 
-_CONFIG_DIR = Path.home() / ".config/kustosz/"
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+PROJECT_DIR = Path(__file__).resolve().parent.parent
+
+BASE_DIR = PROJECT_DIR
+try:
+    BASE_DIR = Path(os.environ["KUSTOSZ_BASE_DIR"])
+except KeyError:
+    pass
 
 import dynaconf  # noqa
 
 settings = dynaconf.DjangoDynaconf(
     __name__,
     settings_files=[
-        BASE_DIR / "settings.yaml",
-        BASE_DIR / ".secrets.yaml",
-        _CONFIG_DIR / "settings.yaml",
-        _CONFIG_DIR / ".secrets.yaml",
+        BASE_DIR / "settings/settings.yaml",
+        BASE_DIR / "settings/.secrets.yaml",
     ],
 )  # noqa
