bd3cec0d606b80331d47bf46ea29084624d8119e
Mirek Długosz <mirek@mirekdlugosz.com>
2025-04-19 13:40:12 +0200

    Introduce Django 5.2 support

--- noxfile.py
+++ noxfile.py
@@ -16,7 +16,7 @@ def lint(session):
 
 
 @nox.session(python=["3.11", "3.12", "3.13"])
-@nox.parametrize("django_ver", ["4.2.20", "5.0.14", "5.1.8"])
+@nox.parametrize("django_ver", ["4.2.20", "5.1.8", "5.2"])
 def tests(session, django_ver):
     session.run(
         "poetry", "install", "--no-interaction", "--extras", "test", external=True
