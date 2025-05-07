ad0bba74cf3b5d043c9154196e04cd6f8dc5befa
Mirek Długosz <mirek@mirekdlugosz.com>
2025-05-07 17:06:35 +0200

    Update Django versions

--- noxfile.py
+++ noxfile.py
@@ -16,7 +16,7 @@ def lint(session):
 
 
 @nox.session(python=["3.11", "3.12", "3.13"])
-@nox.parametrize("django_ver", ["4.2.20", "5.1.8", "5.2"])
+@nox.parametrize("django_ver", ["4.2.21", "5.1.9", "5.2.1"])
 def tests(session, django_ver):
     session.run(
         "poetry", "install", "--no-interaction", "--extras", "test", external=True
